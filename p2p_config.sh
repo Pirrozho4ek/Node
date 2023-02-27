cdd4558f6af7d552a7e04cfb4e0edc7316239526
s.pogorelov <pogorelov.sergey.a@gmail.com>
2023-02-27 21:44:12 +0700

    fix p2p config script

--- p2p_config.sh
+++ p2p_config.sh
@@ -2,17 +2,28 @@
 
 IP=$1
 LADDRSTRING="s/laddr = \"tcp:\/\/0.0.0.0:26656\"/laddr = \"tcp:\/\/"$IP":26656\"/g"
-RESULT="sed -i '' '$LADDRSTRING' $HOME/.entangled/config/config.toml"
-
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  RESULT="sed -i '' '$LADDRSTRING' $HOME/.entangled/config/config.toml"
+else
+  RESULT="sed -i '$LADDRSTRING' $HOME/.entangled/config/config.toml"
+fi
 eval $RESULT
 
 NODESTRING="s/node = \"tcp:\/\/localhost:26657\"/node = \"tcp:\/\/"$IP":26657\"/g"
-RESULT="sed -i '' '$NODESTRING' $HOME/.entangled/config/client.toml"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  RESULT="sed -i '' '$NODESTRING' $HOME/.entangled/config/client.toml"
+else
+  RESULT="sed -i '$NODESTRING' $HOME/.entangled/config/client.toml"
+fi
 
 eval $RESULT
 
 ADDRESS="s/address = \"tcp:\/\/0.0.0.0:1317\"/address = \"tcp:\/\/"$IP":1317\"/g"
-RESULT="sed -i '' '$ADDRESS' $HOME/.entangled/config/app.toml"
+if [[ "$OSTYPE" == "darwin"* ]]; then 
+  RESULT="sed -i '' '$ADDRESS' $HOME/.entangled/config/app.toml"
+else
+  RESULT="sed -i '$ADDRESS' $HOME/.entangled/config/app.toml"
+fi
 
 eval $RESULT
 
